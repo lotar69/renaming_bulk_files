ad42fa6a37f850c5dd18f35e152f4b097e095497
lotar69 <lotardy@gmail.com>
2022-02-18 13:26:17 +0100

    pathlib for dynamic

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import os
+from pathlib import Path
+
+current_path = Path.cwd() / "images"
 
 
 def main():
     i = 0
-    path = r"C:\Users\lotar\Desktop\projets\renaming_bulk_files\images/"
-    for filename in os.listdir(path):
+    for filename in os.listdir(current_path):
         my_dest = "img" + str(i) + ".jpg"
-        my_source = path + filename
-        my_dest = path + my_dest
+        my_source = current_path / filename
+        my_dest = current_path / my_dest
         os.rename(my_source, my_dest)
         i += 1
 
+
+print()
 
 if __name__ == '__main__':
     main()
